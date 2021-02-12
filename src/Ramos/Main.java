552a8d08137af7a2a7d6f2155bd65b584ce3a94e
Kevin Ramos <kevin.jo.ramos@gmail.com>
2021-02-12 10:27:09 -0700

    This commit is for the stretch challenge

--- src/Ramos/Main.java
+++ src/Ramos/Main.java
@@ -5,5 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	    System.out.println("Hello, my name is Kevin.");
         System.out.println("Hello Kevin, My name is Carlos");
+
+        //Stretch Activity.
+        System.out.println("This edit is for the stretch activity.");
+
     }
 }
